af79645a53846c882d9bbd284addabb897d118a3
Patricio Sepúlveda Heise <sep.heise@gmail.com>
2023-05-30 11:04:45 -0300

    Perform request with necessary headers

--- AIAssistant/AIAssistantTests/OpenAIMessageSenderTests.swift
+++ AIAssistant/AIAssistantTests/OpenAIMessageSenderTests.swift
@@ -14,15 +14,21 @@ protocol HTTPClient {
 class OpenAIMessageSender {
     let client: HTTPClient
     let url: URL
+    let apiKey: String
 
-    init(client: HTTPClient, url: URL) {
+    init(client: HTTPClient, url: URL, apiKey: String) {
         self.client = client
         self.url = url
+        self.apiKey = apiKey
     }
 
     func send() {
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = "POST"
+        urlRequest.allHTTPHeaderFields = [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer \(apiKey)"
+        ]
 
         client.lines(for: urlRequest)
     }
@@ -35,25 +41,32 @@ class OpenAIMessageSenderTests: XCTestCase {
         XCTAssertEqual(client.sentRequests, [], "Should not perform any request")
     }
 
-    func test_send_startRequestWithURLAndHTTPMethod() {
+    func test_send_startRequestWithURLAndHTTPMethodAndHeaders() {
         let url = URL(string: "http://any-url.com")!
-        let (sut, client) = makeSUT(url: url)
+        let apiKey = anySecretKey()
+        let (sut, client) = makeSUT(url: url, apiKey: apiKey)
 
         sut.send()
 
         XCTAssertEqual(client.sentRequests.count, 1)
         XCTAssertEqual(client.sentRequests.first?.url, url)
         XCTAssertEqual(client.sentRequests.first?.httpMethod, "POST")
+        XCTAssertEqual(client.sentRequests.first?.value(forHTTPHeaderField: "Content-Type"), "application/json")
+        XCTAssertEqual(client.sentRequests.first?.value(forHTTPHeaderField: "Authorization"), "Bearer \(apiKey)")
     }
 }
 
 // MARK: Helpers
 
-private func makeSUT(url: URL = URL(string: "http://any-url.com")!) -> (sut: OpenAIMessageSender, client: HTTPClientSpy) {
+private func makeSUT(url: URL = URL(string: "http://any-url.com")!, apiKey: String = anySecretKey()) -> (sut: OpenAIMessageSender, client: HTTPClientSpy) {
     let client = HTTPClientSpy()
-    let sut = OpenAIMessageSender(client: client, url: url)
+    let sut = OpenAIMessageSender(client: client, url: url, apiKey: apiKey)
 
     return (sut, client)
+}
+
+private func anySecretKey() -> String {
+    return "some secret key"
 }
 
 // MARK: Test Doubles
