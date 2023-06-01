ada7210e5c61337f57e36a26ea874e16648c1fad
Patricio Sepúlveda Heise <sep.heise@gmail.com>
2023-06-01 09:56:33 -0300

    `URLSessionHTTPClient` deliver error on all invalid cases

--- AIAssistant/AIAssistantTests/URLSessionHTTPClientTests.swift
+++ AIAssistant/AIAssistantTests/URLSessionHTTPClientTests.swift
@@ -15,10 +15,9 @@ class URLSessionHTTPClient: HTTPClient {
         self.session = session
     }
 
-    func lines(from request: URLRequest) async throws -> HTTPClient.LinesStream {
-        do {
-            _ = try await session.bytes(for: request)
-        } catch {
+    func lines(from request: URLRequest) async throws -> LinesStream {
+        guard let (_, response) = try? await session.bytes(for: request),
+              let _ = response as? HTTPURLResponse else {
             throw HTTPClientError.connectivity
         }
 
@@ -31,9 +30,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         URLProtocolStub.startInterceptingRequests()
 
         let urlRequest = URLRequest(url: URL(string: "http://any-url.com")!)
-        let sut = URLSessionHTTPClient()
-        let anyError = NSError(domain: "any error", code: 0)
+        let anyError = anyNSError()
         URLProtocolStub.stub(data: nil, response: nil, error: anyError)
+        let sut = URLSessionHTTPClient()
 
         do {
             _ = try await sut.lines(from: urlRequest)
@@ -44,7 +43,62 @@ class URLSessionHTTPClientTests: XCTestCase {
 
         URLProtocolStub.stopInterceptingRequests()
     }
+
+    func test_lines_deliversErrorOnAllInvalidCases() async {
+        URLProtocolStub.startInterceptingRequests()
+        await resultErrorFor(data: nil, response: nil, error: nil)
+        await resultErrorFor(data: nil, response: nonHTTPURLResponse(), error: nil)
+        await resultErrorFor(data: anyData(), response: nil, error: nil)
+        await resultErrorFor(data: anyData(), response: nil, error: anyNSError())
+        await resultErrorFor(data: nil, response: nonHTTPURLResponse(), error: anyNSError())
+        await resultErrorFor(data: nil, response: anyHTTPURLResponse(), error: anyNSError())
+        await resultErrorFor(data: anyData(), response: nonHTTPURLResponse(), error: anyNSError())
+        await resultErrorFor(data: anyData(), response: anyHTTPURLResponse(), error: anyNSError())
+        await resultErrorFor(data: anyData(), response: nonHTTPURLResponse(), error: nil)
+        URLProtocolStub.stopInterceptingRequests()
+    }
 }
+
+// MARK: - Helper methods
+
+private func resultErrorFor(data: Data?, response: URLResponse?, error: Error?) async {
+    URLProtocolStub.stub(data: data, response: response, error: error)
+    let urlRequest = URLRequest(url: URL(string: "http://any-url.com")!)
+    let sut = URLSessionHTTPClient()
+
+    do {
+        _ = try await sut.lines(from: urlRequest)
+        XCTFail("Expected error: \(HTTPClientError.connectivity)")
+    } catch {
+        XCTAssertEqual(error as? HTTPClientError, .connectivity)
+    }
+}
+
+private func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
+private func anyData() -> Data {
+    return Data("any data".utf8)
+}
+
+private func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
+private func anyHTTPURLResponse() -> HTTPURLResponse {
+    return HTTPURLResponse(url: anyURL(), statusCode: 200, httpVersion: nil, headerFields: nil)!
+}
+
+private func nonHTTPURLResponse() -> URLResponse {
+    return URLResponse(url: anyURL(), mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+}
+
+private func successfulHTTPURLResponse() -> HTTPURLResponse {
+    return HTTPURLResponse(url: anyURL(), statusCode: 200, httpVersion: nil, headerFields: nil)!
+}
+
+// MARK: - Test doubles
 
 class URLProtocolStub: URLProtocol {
     private static var stub: Stub?
