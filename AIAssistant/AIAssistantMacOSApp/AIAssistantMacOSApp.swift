0fac2fb85342d95adcfb73918a57b6408560b125
Patricio Sepúlveda Heise <sep.heise@gmail.com>
2023-06-09 21:08:02 -0300

    Add MacOS App target

---
+++ AIAssistant/AIAssistantMacOSApp/AIAssistantMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIAssistantMacOSAppApp.swift
+//  AIAssistantMacOSApp
+//
+//  Created by Patricio Sepúlveda Heise on 09-06-23.
+//
+
+import SwiftUI
+
+@main
+struct AIAssistantMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
