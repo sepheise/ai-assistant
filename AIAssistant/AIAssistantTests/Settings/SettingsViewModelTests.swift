e1b54d62c56b264f4e426ea95a6287cef773efce
Patricio Sepúlveda Heise <sep.heise@gmail.com>
2023-07-03 23:08:58 -0300

    Set error message on API key load failure

--- AIAssistant/AIAssistantTests/Settings/SettingsViewModelTests.swift
+++ AIAssistant/AIAssistantTests/Settings/SettingsViewModelTests.swift
@@ -9,7 +9,8 @@ import XCTest
 import AIAssistant
 
 class SettingsViewModel {
-    @Published var openAIApiKey: String = ""
+    var openAIApiKey: String = ""
+    var errorMessage: String = ""
     private let apiKeyLoader: APIKeyLoader
 
     init(apiKeyLoader: APIKeyLoader) {
@@ -20,14 +21,14 @@ class SettingsViewModel {
         do {
             openAIApiKey = try apiKeyLoader.load()
         } catch {
-
+            errorMessage = "Couldn't load API Key"
         }
     }
 }
 
 class SettingsViewModelTests: XCTestCase {
-    func test_onAppear_loadsAndSetOpenAIKey() {
-        let apiKeyLoaderSpy = APIKeyLoaderSpy()
+    func test_onAppear_loadsAndSetOpenAIKeyOnSuccessfulLoad() {
+        let apiKeyLoaderSpy = APIKeyLoaderSpy(result: .success("testKey"))
         let sut = SettingsViewModel(apiKeyLoader: apiKeyLoaderSpy)
 
         sut.onAppear()
@@ -35,13 +36,29 @@ class SettingsViewModelTests: XCTestCase {
         XCTAssertEqual(apiKeyLoaderSpy.loadCallsCount, 1)
         XCTAssertEqual(sut.openAIApiKey, "testKey")
     }
+
+    func test_onAppear_setsErrorMessageOnLoadFailure() {
+        let apiKeyLoaderSpy = APIKeyLoaderSpy(result: .failure(NSError(domain: "any error", code: 0)))
+        let sut = SettingsViewModel(apiKeyLoader: apiKeyLoaderSpy)
+
+        sut.onAppear()
+
+        XCTAssertEqual(apiKeyLoaderSpy.loadCallsCount, 1)
+        XCTAssertEqual(sut.openAIApiKey, "")
+        XCTAssertEqual(sut.errorMessage, "Couldn't load API Key")
+    }
 }
 
 class APIKeyLoaderSpy: APIKeyLoader {
+    var result: Result<String, Error>
     var loadCallsCount: Int = 0
+
+    init(result: Result<String, Error>) {
+        self.result = result
+    }
 
     func load() throws -> String {
         loadCallsCount += 1
-        return "testKey"
+        return try result.get()
     }
 }
